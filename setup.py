bbc5484620d1467ff9e237ff61f8f2c5dc934386
T. Kim Nguyen <kim@sixfeetup.com>
2020-02-01 14:08:48 +0000

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='sixfeetup.pmdashboard',
-    version='1.0a1',
+    version='1.0a2.dev0',
     description="a dashboard for Six Feet Up project managers",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
